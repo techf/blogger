ba174370c4c315625e2a37170b7f0fc003ba4d58
buntu <teklufe@gmail.com>
2014-04-15 22:14:07 +0200

    form-based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
 	def index
 		@articles = Article.all
 	end
@@ -6,5 +7,37 @@ class ArticlesController < ApplicationController
 	def show
 	#	render json: params	
 		@article = Article.find(params[:id])	
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		@article.save
+		flash[:success] = "successfully created article '#{@article.title}'"
+
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.delete
+		flash[:info] = "the article '#{@article.title}' successfully deleted."
+
+		redirect_to articles_path
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+	
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+		flash[:success] = "Article '#{@article.title}' is successfully updated"
+
+		redirect_to article_path(@article)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Blogger2::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 	resources :articles
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'articles#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
